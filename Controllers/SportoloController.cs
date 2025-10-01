2f829c5c31812605765197c7083d23fb3fa37754
BordasDaniel <bordas.daniel0124@gmail.com>
2025-10-01 16:36:28 +0200

    Megjelenítés fixálva. Dizájnolások...

--- Controllers/SportoloController.cs
+++ Controllers/SportoloController.cs
@@ -25,20 +25,52 @@ namespace WEB_App__MVC_.ViewControllers
                 MySqlCommand mySqlCommand = new(sql, SQLConnection);
                 MySqlDataReader mySqlDataReader = mySqlCommand.ExecuteReader();
 
+                int idOrd = -1, nameOrd = -1, dobOrd = -1, compOrd = -1, photoOrd = -1;
+                if (mySqlDataReader.FieldCount > 0)
+                {
+                    int GetOrdinalSafe(MySqlDataReader reader, string oszlopNev)
+                    {
+                        try
+                        {
+                            return reader.GetOrdinal(oszlopNev);
+                        } catch (IndexOutOfRangeException)
+                        {
+                            return -1;
+                        }
+                    }
+
+                    idOrd = GetOrdinalSafe(mySqlDataReader, "Id");
+                    nameOrd = GetOrdinalSafe(mySqlDataReader, "Name");
+                    dobOrd = GetOrdinalSafe(mySqlDataReader, "DateOfBirth");
+                    compOrd = GetOrdinalSafe(mySqlDataReader, "CompetitionNumbers");
+                    photoOrd = GetOrdinalSafe(mySqlDataReader, "Photo");
+                }
+
                 while (mySqlDataReader.Read())
                 {
+                    int id = 0;
+                    if (idOrd >= 0 && !mySqlDataReader.IsDBNull(idOrd)) id = mySqlDataReader.GetInt32(idOrd);
+
+                    string name = string.Empty;
+                    if (nameOrd >= 0 && !mySqlDataReader.IsDBNull(nameOrd)) name = mySqlDataReader.GetString(nameOrd);
+
+                    DateTime dob = DateTime.MinValue;
+                    if (dobOrd >= 0 && !mySqlDataReader.IsDBNull(dobOrd)) dob = mySqlDataReader.GetDateTime(dobOrd);
+
+                    string comp = string.Empty;
+                    if (compOrd >= 0 && !mySqlDataReader.IsDBNull(compOrd)) comp = mySqlDataReader.GetString(compOrd);
+
                     byte[] photo = null;
-                    int imgOrd = mySqlDataReader.GetOrdinal("Photo");
-                    photo = (byte[])mySqlDataReader["Photo"];
+                    if (photoOrd >= 0) photo = (byte[])mySqlDataReader.GetValue(photoOrd);
 
                     sportolok.Add(new Sportolo()
                     {
-                        Id = mySqlDataReader.GetInt32("id"),
-                        Name = mySqlDataReader.GetString("name"),
-                        DateOfBirth = mySqlDataReader.GetDateTime("birth"),
-                        CompetitionNumbers = mySqlDataReader.GetString("competitionNumbers"),
+                        Id = id,
+                        Name = name,
+                        DateOfBirth = dob,
+                        CompetitionNumbers = comp,
                         Photo = photo
-                    }) ;
+                    });
                 }
                 SQLConnection.Close();
 
